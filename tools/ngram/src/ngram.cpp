0f273c47c9c3b4372fb7ed0516542ef749aa4b96
Amir Bawab <amirbawab@gmail.com>
2017-09-23 14:00:56 -0400

    Parse data from input

--- tools/ngram/src/ngram.cpp
+++ tools/ngram/src/ngram.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <pugixml.hpp>
+#include <sstream>
+#include <vector>
 
 int main() {
+
+    // Read input
+    std::stringstream xmlContent;
+    std::string lineInput;
+    while (getline(std::cin, lineInput)) {
+        xmlContent << lineInput;
+    }
+
+    // Parse xml
     pugi::xml_document doc;
-    std::cout << "Hello world" << std::endl;
+    doc.load_string(xmlContent.str().c_str());
+    pugi::xml_node dialog = doc.child("dialog");
+
+    // Loop over conversations
+    for (pugi::xml_node sTag = dialog.child("s"); sTag; sTag = sTag.next_sibling("s")) {
+
+        // Loop over utterances
+        for (pugi::xml_node uttTag = sTag.child("utt"); uttTag; uttTag = uttTag.next_sibling("utt")) {
+            std::cout << uttTag.attribute("uid").value() << ": " << uttTag.child_value() << std::endl;
+        }
+    }
+
+    std::vector<std::vector<int>> wordWordCount;
     return 0;
 }
