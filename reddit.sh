5b864ca7f95035cf0d88d4626d33756783684a35
Amir Bawab <amirbawab@gmail.com>
2017-09-27 00:38:30 -0400

    Working on pipeline

--- reddit.sh
+++ reddit.sh
@@ -7,5 +7,41 @@ echo " / /__| (_) | |  | |_) | |_| \__ |_____| || (_) | (_) | \__ \ "
 echo " \____/\___/|_|  | .__/ \__,_|___/      \__\___/ \___/|_|___/ "
 echo "                 |_|                                          "
 
-echo "Starting the pipeline"
+# Global variables
+g_fixedJSON="/tmp/tmp1.json"
+g_finalXML="/tmp/data.xml"
 
+# Load JSON
+echo "Starting the pipeline"
+echo ">> Select JSON file to process: "
+#read jsonFile
+jsonFile="/tmp/json/raw/french_comments_part.json"
+
+# Fix JSON syntax
+echo 
+echo ">> Adjusting JSON format ..."
+echo ">>>> Removing ${g_fixedJSON} (if any)"
+rm -f ${g_fixedJSON}
+echo ">>>> Generating new JSON file"
+./tools/bash/prettyGCloud.sh "${jsonFile}" "${g_fixedJSON}"
+
+# Start preprocessing the data
+echo 
+echo ">> Pre-processing JSON file ..."
+echo ">>>> Delete lines matching [deleted] or [removed] ..."
+sed -i '/\[\(deleted\|removed\)\]/d' "${g_fixedJSON}"
+echo ">>>> Delete lines matching 'I am a bot' or 'I'm a bot'"
+sed -i '/I am a bot/d' "${g_fixedJSON}"
+sed -i "/I'm a bot/d" "${g_fixedJSON}"
+echo ">>>> Remove post quotes"
+echo "TO BE ADDED"
+echo ">>>> Removing non-french sentences ..."
+echo "TO BE ADDED"
+
+# Start the converting the data from JSON to XML
+echo
+echo ">> Converting JSON to XML (this might take few minutes...)"
+./tools/cpp/bin/json2xml -i "${g_fixedJSON}" -o "${g_finalXML}" -m 2 -s
+
+# End message
+echo "!!!End of pipeline!!!"
